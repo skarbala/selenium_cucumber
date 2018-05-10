26538a19cb8a34e8ca7c816e511790880891cb24
skarbala <skarbala.martin@gmail.com>
2018-05-10 22:12:13 +0200

    added method for checking description

--- src/test/java/pages/ClickMeBabyPage.java
+++ src/test/java/pages/ClickMeBabyPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import base.WebDriverSingleton;
 import models.Sin;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -50,4 +51,7 @@ public class ClickMeBabyPage {
         return clickDescriptiton.getText();
     }
 
+    public void checkClickDescription(String expectedDescription) {
+        Assert.assertEquals(expectedDescription, getClickDescription());
+    }
 }
