d5a9a511a4cd7c645e253cff4c1775b2ab15b34d
martin.skarbala <skarbala.martin@gmail.com>
2018-05-21 14:51:43 +0200

    fix: author input method

--- src/test/java/pages/SinCityPage.java
+++ src/test/java/pages/SinCityPage.java
@@ -1,8 +1,9 @@
 package pages;
 
-import base.WebDriverSingleton;
-import enumerators.SinType;
-import models.Sin;
+import static base.TestBase.BASE_URL;
+
+import java.util.List;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,103 +13,102 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
-import static base.TestBase.BASE_URL;
+import base.WebDriverSingleton;
+import enumerators.SinType;
+import models.Sin;
 
 public class SinCityPage {
 
-    private WebDriver driver;
+  private static final String PAGE_URL = "sincity.php";
+  private WebDriver driver;
+  @FindBy(name = "title")
+  private WebElement titleInput;
 
-    private static final String PAGE_URL = "sincity.php";
+  @FindBy(name = "author")
+  private WebElement authorInput;
 
-    @FindBy(name = "title")
-    private WebElement titleInput;
+  @FindBy(name = "message")
+  private WebElement messageInput;
 
-    @FindBy(name = "author")
-    private WebElement authorInput;
+  @FindBy(xpath = "//button[@type='submit']")
+  private WebElement confessButton;
 
-    @FindBy(name = "message")
-    private WebElement messageInput;
+  @FindBy(css = "div.sinsListArea")
+  private WebElement sinListSection;
 
-    @FindBy(xpath = "//button[@type='submit']")
-    private WebElement confessButton;
+  public SinCityPage() {
+    driver = WebDriverSingleton.getInstance().getDriver();
+    PageFactory.initElements(driver, this);
+  }
 
-    @FindBy(css = "div.sinsListArea")
-    private WebElement sinListSection;
+  public void openPage() {
+    driver.get(BASE_URL + PAGE_URL);
+  }
 
-    public SinCityPage() {
-        driver = WebDriverSingleton.getInstance().getDriver();
-        PageFactory.initElements(driver, this);
+  public void fillSinInformation(Sin sin) {
+    fillSinTitle(sin.getTitle());
+    fillSinAuthor(sin.getAuthor());
+    fillSinMessage(sin.getMessage());
+  }
+
+  public void fillSinTitle(String title) {
+    titleInput.sendKeys(title);
+  }
+
+  public void fillSinAuthor(String author) {
+    authorInput.sendKeys(author);
+  }
+
+  public void fillSinMessage(String message) {
+    titleInput.sendKeys(message);
+  }
+
+  public void markTag(List<SinType> tags) {
+    for (SinType tag : tags) {
+      driver.findElement(By.xpath("//input[@value='" + tag.getXpathValue() + "']")).click();
     }
+  }
 
-    public void openPage() {
-        driver.get(BASE_URL + PAGE_URL);
-    }
+  public void confessSin() {
+    confessButton.click();
+  }
 
-    public void fillSinInformation(Sin sin) {
-        fillSinTitle(sin.getTitle());
-        fillSinAuthor(sin.getAuthor());
-        fillSinMessage(sin.getMessage());
+  public void checkSinStatus(Sin spiderSin) {
+    //1.chcem najst element hriechu
+    WebElement actualSin = getSinFromListElement(spiderSin);
+    //2.chcem z neho vytiahnut text
+    String actualText = actualSin.findElement(By.cssSelector("p")).getText().trim();
+    //3.porovnat ocakavane a skutocne
+    if (spiderSin.isForgiven()) {
+      Assert.assertEquals("forgiven", actualText);
+    } else {
+      Assert.assertEquals("pending", actualText);
     }
+  }
 
-    public void fillSinTitle(String title) {
-        titleInput.sendKeys(title);
-    }
+  private WebElement getSinFromListElement(Sin spiderSin) {
+    WebElement listOfSins = driver.findElement(By.cssSelector("ul.list-of-sins"));
+    return listOfSins.findElement(By.xpath("./li[contains(text(),'" + spiderSin.getTitle() + "')]"));
+  }
 
-    public void fillSinAuthor(String author) {
-        titleInput.sendKeys(author);
-    }
+  public void openDetail(Sin spiderSin) {
+    getSinFromListElement(spiderSin).click();
+  }
 
-    public void fillSinMessage(String message) {
-        titleInput.sendKeys(message);
-    }
-
-    public void markTag(List<SinType> tags) {
-        for (SinType tag : tags) {
-            driver.findElement(By.xpath("//input[@value='" + tag.getXpathValue() + "']")).click();
-        }
-    }
-
-    public void confessSin() {
-        confessButton.click();
-    }
-
-    public void checkSinStatus(Sin spiderSin) {
-        //1.chcem najst element hriechu
-        WebElement actualSin = getSinFromListElement(spiderSin);
-        //2.chcem z neho vytiahnut text
-        String actualText = actualSin.findElement(By.cssSelector("p")).getText().trim();
-        //3.porovnat ocakavane a skutocne
-        if (spiderSin.isForgiven()) {
-            Assert.assertEquals("forgiven", actualText);
-        } else {
-            Assert.assertEquals("pending", actualText);
-        }
-    }
-
-    private WebElement getSinFromListElement(Sin spiderSin) {
-        WebElement listOfSins = driver.findElement(By.cssSelector("ul.list-of-sins"));
-        return listOfSins.findElement(By.xpath("./li[contains(text(),'" + spiderSin.getTitle() + "')]"));
-    }
-
-    public void openDetail(Sin spiderSin) {
-        getSinFromListElement(spiderSin).click();
-    }
-
-    public void checkDetail(Sin spiderSin) {
-        //1.najst element detailu
-        WebElement sinDetail = driver.findElement(By.cssSelector("div.detail"));
-        //2.pockat na hriech
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.textToBePresentInElement(
-                        sinDetail.findElement(By.cssSelector("p")),
-                        spiderSin.getMessage())
-                );
-        //3.overit message, author, title
-        String actualTitle = sinDetail.findElement(By.cssSelector("h4")).getText();
-        Assert.assertTrue(actualTitle.contains(spiderSin.getTitle()));
-        Assert.assertTrue(actualTitle.contains(spiderSin.getAuthor()));
-    }
+  public void checkDetail(Sin spiderSin) {
+    //1.najst element detailu
+    WebElement sinDetail = driver.findElement(By.cssSelector("div.detail"));
+    //2.pockat na hriech
+    new WebDriverWait(driver, 10)
+        .until(ExpectedConditions.textToBePresentInElement(
+            sinDetail.findElement(By.cssSelector("p")),
+            spiderSin.getMessage()
+            )
+        );
+    //3.overit message, author, title
+    String actualTitle = sinDetail.findElement(By.cssSelector("h4")).getText();
+    Assert.assertTrue(actualTitle.contains(spiderSin.getTitle()));
+    Assert.assertTrue(actualTitle.contains(spiderSin.getAuthor()));
+  }
 
 }
