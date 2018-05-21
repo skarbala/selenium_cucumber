bea599ecb8ac05b5635ffcfdf935f9c26146fcc4
skarbala <skarbala.martin@gmail.com>
2018-05-21 18:43:50 +0200

    added method for checking element in the list of sins

--- src/test/java/pages/SinCityPage.java
+++ src/test/java/pages/SinCityPage.java
@@ -19,96 +19,103 @@ import models.Sin;
 
 public class SinCityPage {
 
-  private static final String PAGE_URL = "sincity.php";
-  private WebDriver driver;
-  @FindBy(name = "title")
-  private WebElement titleInput;
+    private static final String PAGE_URL = "sincity.php";
+    private WebDriver driver;
+    @FindBy(name = "title")
+    private WebElement titleInput;
 
-  @FindBy(name = "author")
-  private WebElement authorInput;
+    @FindBy(name = "author")
+    private WebElement authorInput;
 
-  @FindBy(name = "message")
-  private WebElement messageInput;
+    @FindBy(name = "message")
+    private WebElement messageInput;
 
-  @FindBy(xpath = "//button[@type='submit']")
-  private WebElement confessButton;
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement confessButton;
 
-  @FindBy(css = "div.sinsListArea")
-  private WebElement sinListSection;
+    @FindBy(css = "div.sinsListArea")
+    private WebElement sinListSection;
 
-  public SinCityPage() {
-    driver = WebDriverSingleton.getInstance().getDriver();
-    PageFactory.initElements(driver, this);
-  }
+    @FindBy(css = "ul.list-of-sins")
+    private WebElement listOfSins;
 
-  public void openPage() {
-    driver.get(BASE_URL + PAGE_URL);
-  }
-
-  public void fillSinInformation(Sin sin) {
-    fillSinTitle(sin.getTitle());
-    fillSinAuthor(sin.getAuthor());
-    fillSinMessage(sin.getMessage());
-  }
-
-  public void fillSinTitle(String title) {
-    titleInput.sendKeys(title);
-  }
-
-  public void fillSinAuthor(String author) {
-    authorInput.sendKeys(author);
-  }
-
-  public void fillSinMessage(String message) {
-    titleInput.sendKeys(message);
-  }
-
-  public void markTag(List<SinType> tags) {
-    for (SinType tag : tags) {
-      driver.findElement(By.xpath("//input[@value='" + tag.getXpathValue() + "']")).click();
+    public SinCityPage() {
+        driver = WebDriverSingleton.getInstance().getDriver();
+        PageFactory.initElements(driver, this);
     }
-  }
 
-  public void confessSin() {
-    confessButton.click();
-  }
-
-  public void checkSinStatus(Sin spiderSin) {
-    //1.chcem najst element hriechu
-    WebElement actualSin = getSinFromListElement(spiderSin);
-    //2.chcem z neho vytiahnut text
-    String actualText = actualSin.findElement(By.cssSelector("p")).getText().trim();
-    //3.porovnat ocakavane a skutocne
-    if (spiderSin.isForgiven()) {
-      Assert.assertEquals("forgiven", actualText);
-    } else {
-      Assert.assertEquals("pending", actualText);
+    public void openPage() {
+        driver.get(BASE_URL + PAGE_URL);
     }
-  }
 
-  private WebElement getSinFromListElement(Sin spiderSin) {
-    WebElement listOfSins = driver.findElement(By.cssSelector("ul.list-of-sins"));
-    return listOfSins.findElement(By.xpath("./li[contains(text(),'" + spiderSin.getTitle() + "')]"));
-  }
+    public void fillSinInformation(Sin sin) {
+        fillSinTitle(sin.getTitle());
+        fillSinAuthor(sin.getAuthor());
+        fillSinMessage(sin.getMessage());
+    }
 
-  public void openDetail(Sin spiderSin) {
-    getSinFromListElement(spiderSin).click();
-  }
+    public void fillSinTitle(String title) {
+        titleInput.sendKeys(title);
+    }
 
-  public void checkDetail(Sin spiderSin) {
-    //1.najst element detailu
-    WebElement sinDetail = driver.findElement(By.cssSelector("div.detail"));
-    //2.pockat na hriech
-    new WebDriverWait(driver, 10)
-        .until(ExpectedConditions.textToBePresentInElement(
-            sinDetail.findElement(By.cssSelector("p")),
-            spiderSin.getMessage()
-            )
-        );
-    //3.overit message, author, title
-    String actualTitle = sinDetail.findElement(By.cssSelector("h4")).getText();
-    Assert.assertTrue(actualTitle.contains(spiderSin.getTitle()));
-    Assert.assertTrue(actualTitle.contains(spiderSin.getAuthor()));
-  }
+    public void fillSinAuthor(String author) {
+        authorInput.sendKeys(author);
+    }
+
+    public void fillSinMessage(String message) {
+        titleInput.sendKeys(message);
+    }
+
+    public void markTag(List<SinType> tags) {
+        for (SinType tag : tags) {
+            driver.findElement(By.xpath("//input[@value='" + tag.getXpathValue() + "']")).click();
+        }
+    }
+
+    public void confessSin() {
+        confessButton.click();
+    }
+
+    public void checkSinStatus(Sin sin) {
+        //1.chcem najst element hriechu
+        WebElement actualSin = getSinFromListElement(sin);
+        //2.chcem z neho vytiahnut text
+        String actualText = actualSin.findElement(By.cssSelector("p")).getText().trim();
+        //3.porovnat ocakavane a skutocne
+        if (sin.isForgiven()) {
+            Assert.assertEquals("forgiven", actualText);
+        } else {
+            Assert.assertEquals("pending", actualText);
+        }
+    }
+
+    private WebElement getSinFromListElement(Sin sin) {
+        return listOfSins.findElement(By.xpath("./li[contains(text(),'" + sin.getTitle() + "')]"));
+    }
+
+    private void checkSinVisibleInTheList(Sin sin) {
+        Assert.assertTrue(listOfSins.findElements(
+                By.xpath("./li[contains(text(),'" + sin.getTitle() + "')]")).size() > 0);
+    }
+
+    public void openDetail(Sin sin) {
+        getSinFromListElement(sin).click();
+    }
+
+    public void checkDetail(Sin sin) {
+        //1.najst element detailu
+        WebElement sinDetail = driver.findElement(By.cssSelector("div.detail"));
+        //2.pockat na hriech
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.textToBePresentInElement(
+                        sinDetail.findElement(By.cssSelector("p")),
+                        sin.getMessage()
+                        )
+                );
+        //3.overit message, author, title
+        String actualTitle = sinDetail.findElement(By.cssSelector("h4")).getText();
+        Assert.assertTrue(actualTitle.contains(sin.getTitle()));
+        Assert.assertTrue(actualTitle.contains(sin.getAuthor()));
+    }
 
 }
