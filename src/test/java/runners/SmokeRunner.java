176364e65a4da15e11c2884a1e2019c6783edb8b
skarbala <skarbala.martin@gmail.com>
2018-06-20 16:36:35 +0200

    added format to smoke runner, pom.xml updated

--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"steps", "hooks"},
         tags = {"@smoke"},
-        junit = "--step-notifications"
+        junit = "--step-notifications",
+        format = {"json:target/reports/cucumber-reports/SmokeRunner.json"}
 )
 public class SmokeRunner {
 }
